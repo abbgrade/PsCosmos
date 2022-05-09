0dd0d65866fabe0dbc2fcec9f26464a0fa021ae2
Steffen <skampmann@munichre.com>
2022-05-09 16:26:53 +0200

    updated psbuild tasks

--- .build.ps1
+++ .build.ps1
@@ -40,3 +40,29 @@ task UpdateBuildTasks {
 		-Uri 'https://raw.githubusercontent.com/abbgrade/PsBuildTasks/main/DotNet/Build.Tasks.ps1' `
 		-OutFile "$PSScriptRoot\tasks\Build.Tasks.ps1"
 }
+
+task UpdateValidationWorkflow {
+    Invoke-WebRequest `
+        -Uri 'https://raw.githubusercontent.com/abbgrade/PsBuildTasks/main/GitHub/build-validation-matrix.yml' `
+        -OutFile "$PSScriptRoot\.github\workflows\build-validation.yml"
+}
+
+task UpdateReleaseWorkflow {
+    Invoke-WebRequest `
+        -Uri 'https://raw.githubusercontent.com/abbgrade/PsBuildTasks/main/GitHub/release-windows.yml' `
+        -OutFile "$PSScriptRoot\.github\workflows\release.yml"
+}
+
+task UpdatePreReleaseWorkflow {
+    Invoke-WebRequest `
+        -Uri 'https://raw.githubusercontent.com/abbgrade/PsBuildTasks/main/GitHub/pre-release-windows.yml' `
+        -OutFile "$PSScriptRoot\.github\workflows\pre-release.yml"
+}
+
+task UpdateVsCodeTasks {
+    Invoke-WebRequest `
+        -Uri 'https://raw.githubusercontent.com/abbgrade/PsBuildTasks/main/VsCode\tasks.json' `
+        -OutFile "$PSScriptRoot\.vscode\tasks.json"
+}
+
+task UpdatePsBuildTasks -Jobs UpdateBuildTasks, UpdateValidationWorkflow, UpdatePreReleaseWorkflow, UpdateReleaseWorkflow, UpdateVsCodeTasks
